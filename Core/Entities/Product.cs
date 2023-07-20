fce552a8c17ed08553f643231dea9fb36cf08a8f
firstvf <vfirst1vf@gmail.com>
2023-07-20 13:53:50 +0300

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Остановился на установке Microsoft.EntityFrameworkCore.Design. 14 серия 7:10 
+    }
+}
