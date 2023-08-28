0c9955ba107b64f2a429fb05137e9ff85758d205
firstvf <vfirst1vf@gmail.com>
2023-08-28 22:10:51 +0300

    end 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -37,6 +38,10 @@ export class ShopService {
         }))
   }
 
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + "products/" + id);
+  }
+
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + "products/brands");
   }
@@ -44,4 +49,6 @@ export class ShopService {
   getTypes() {
     return this.http.get<IType[]>(this.baseUrl + "products/types");
   }
+
+
 }
