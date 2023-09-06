526d4c43fb98ecb35d07606ca0a2e9f23fbf1b0e
firstvf <vfirst1vf@gmail.com>
2023-09-06 15:03:36 +0300

    14, basket

--- client/src/app/core/services/busy.service.ts
+++ client/src/app/core/services/busy.service.ts
@@ -10,7 +10,7 @@ export class BusyService {
   constructor(private spinnerService: NgxSpinnerService) { }
 
   busy() {
-    this.busyRequestCount++;
+    this.busyRequestCount ++;
     this.spinnerService.show(undefined, {
       type: 'timer',
       bdColor: 'rgba(255,255,255,0.7',
